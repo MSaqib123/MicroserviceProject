457a09a0045f291f3c94a97236fb9fc2157539b9
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-08-06 07:56:55 +0500

    002 Create Request and Response DTO

---
+++ Mango.Web/Models/RequestDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Mango.Web.Models.SD;
+
+namespace Mango.Web.Models
+{
+    public class RequestDto
+    {
+        public ApiType Result { get; set; } = ApiType.GET;
+        public bool IsSuccess { get; set; } = true;
+        public string Message { get; set; } = "";
+    }
+}
---
+++ Mango.Web/Utility/SD.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Web.Models
+{
+    public class SD
+    {
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}
