feb448eb1756a86d723a2cf71d72f84bc66dc076
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-08-07 08:04:24 +0500

    007 IAuth Service

---
+++ Mango.Services.AuthAPI/Services/AuthService.cs
@@ -0,0 +1,38 @@
+﻿using Mango.Services.AuthAPI.Data;
+using Mango.Services.AuthAPI.Models;
+using Mango.Services.AuthAPI.Models.Dto;
+using Mango.Services.AuthAPI.Services.IServices;
+using Microsoft.AspNetCore.Identity;
+
+namespace Mango.Services.AuthAPI.Services
+{
+    public class AuthService : IAuthService
+    {
+
+        private readonly AppDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        //private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+        public AuthService(AppDbContext db,
+            //IJwtTokenGenerator jwtTokenGenerator,
+            UserManager<ApplicationUser> userManager, 
+            RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            //_jwtTokenGenerator = jwtTokenGenerator;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDto> Register(RegistrationRequestDto registrationRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Mango.Services.AuthAPI/Services/IServices/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Mango.Services.AuthAPI.Models.Dto;
+using Microsoft.AspNetCore.Identity.Data;
+
+namespace Mango.Services.AuthAPI.Services.IServices
+{
+    public interface IAuthService
+    {
+        Task<UserDto> Register(RegistrationRequestDto registrationRequestDto);
+        Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+    }
+}
