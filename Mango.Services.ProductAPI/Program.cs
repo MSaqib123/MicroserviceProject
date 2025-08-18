f2adae9525906b7da138dd5f4834c4c46cf10148
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-08-18 12:07:43 +0500

    [Section 5] 001 Product API Base Setup 002 Assignment Product API 003 Assignment Product API in Action

---
+++ Mango.Services.ProductAPI/Program.cs
@@ -0,0 +1,39 @@
+using AutoMapper;
+using Mango.Services.ProductAPI.Extensions;
+using Mango.Services.ProductAPI;
+using Mango.Services.ProductAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+
+builder.AddSwaggerSettings();
+builder.AddAppAuthentication();
+
+
+var app = builder.Build();
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    if (!app.Environment.IsDevelopment())
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Cart API");
+        c.RoutePrefix = string.Empty;
+    }
+});
+//}
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
