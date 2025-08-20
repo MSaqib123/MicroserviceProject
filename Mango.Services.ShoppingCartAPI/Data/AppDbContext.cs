b59c93250528c815c9bd1eb1a1c8c359a839d2b8
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-08-20 15:27:56 +0500

    003 Basic API Setup

---
+++ Mango.Services.ShoppingCartAPI/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Mango.Services.ProductAPI.Models;
+using Mango.Services.ShoppingCartAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace Mango.Services.ShoppingCartAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CartHeader> CartHeaders { get; set; }
+        public DbSet<CartDetails> CartDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
